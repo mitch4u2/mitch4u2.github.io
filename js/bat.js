3dae0f0182f647b97d95751c998b9f359e04f4bc
Mohamed Hajjej <mohamed.hajjej@esprit.tn>
2020-02-21 15:52:07 +0100

    spint 1 finished added the sound to the planet

--- js/bat.js
+++ js/bat.js
@@ -279,5 +279,61 @@ var electric = function () {
   canvas.style.display='none';
   // canvas2.style.display = 'none';
 }();
+
+//Astraunot Cord
          
-        
+const cordCanvas = document.getElementById('cord');
+const ctx = cordCanvas.getContext('2d');
+
+let y1 = 160;
+let y2 = 100;
+let y3 = 100;
+
+let y1Forward = true;
+let y2Forward = false;
+let y3Forward = true;
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+    ctx.beginPath();
+    ctx.moveTo(130, 170);
+    ctx.bezierCurveTo(250, y1, 345, y2, 400, y3);
+
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 8;
+    ctx.stroke();
+
+
+    if (y1 === 100) {
+        y1Forward = true;
+    }
+
+    if (y1 === 300) {
+        y1Forward = false;
+    }
+
+    if (y2 === 100) {
+        y2Forward = true;
+    }
+
+    if (y2 === 310) {
+        y2Forward = false;
+    }
+
+    if (y3 === 100) {
+        y3Forward = true;
+    }
+
+    if (y3 === 317) {
+        y3Forward = false;
+    }
+
+    y1Forward ? y1 += 1 : y1 -= 1;
+    y2Forward ? y2 += 1 : y2 -= 1;
+    y3Forward ? y3 += 1 : y3 -= 1;
+}
+
+// drawVisor();
+animate();      
